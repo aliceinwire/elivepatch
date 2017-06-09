fff4486b1640ad2420c9d237deef35679a8f5967
Alice Ferrazzi <alicef@gentoo.org>
2017-06-09 03:35:21 +0000

    added setup file

---
+++ elivepatch/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elivepatch',
+    version='0.1',
+    description='Patching differents gentoo distributions',
+    url='https://gentoo.org',
+    author='Alice Ferrazzi',
+    author_email='alice.ferrazzi@gmail.com',
+    license='GNU GPLv2',
+    packages=find_packages(),
+)
