200998af990f6f0ffb382a18a166d7415b266f46
Alice Ferrazzi <alicef@gentoo.org>
2017-06-14 06:40:34 +0900

    fixed name in elivepatch_server setup.py

--- elivepatch_server/setup.py
+++ elivepatch_server/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='elivepatch',
+    name='elivepatch_server',
     version='0.01',
     description='Patching differents gentoo distributions',
     url='https://gentoo.org',
