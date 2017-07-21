3f5cfd9213bd165b3a0c75ce13d83be7372be3b2
Alice Ferrazzi <alicef@gentoo.org>
2017-07-21 23:00:26 +0900

    fixed livepatch output name

--- elivepatch_server/resources/dispatcher.py
+++ elivepatch_server/resources/dispatcher.py
@@ -109,7 +109,7 @@ class SendLivePatch(Resource):
         args['UUID'] = check_uuid(args['UUID'])
         uuid_dir = get_uuid_dir(args['UUID'])
 
-        livepatch_full_path = os.path.join(uuid_dir, 'kpatch-'+ str(args['UUID']) + '-livepatch.ko')
+        livepatch_full_path = os.path.join(uuid_dir, 'kpatch-01.ko')
         try:
             with open(livepatch_full_path, 'rb') as fp:
                 response = make_response(fp.read())
